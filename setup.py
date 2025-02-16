7c7e4371a649d9be85124a8ebda23b04f876c06a
Tharakeshavan Parthasarathy <ptharak01@gmail.com>
2025-02-17 02:23:39 +0800

    setup changes

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     description="A logging handler that sends log messages to Discord via webhooks.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author="Tharakeshavan",
+    author="Tharakeshavan Parthasarathy",
     author_email="ptharak01@gmail.com",
-    url="https://github.com/Tharak01/Discord-Logger",
+    url="https://github.com/Tharak01/Discord-Logger-Handler",
     packages=find_packages(),
     install_requires=[
         "requests>=2.20.0",
