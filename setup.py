bdd48357c10f03e53935f0ccd5783c0a22e00424
Tharakeshavan Parthasarathy <ptharak01@gmail.com>
2025-02-27 01:01:15 +0800

    setup fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="discord-logger-handler",
-    version="0.1.1",
+    version="0.1.2",
     description="A logging handler that sends log messages to Discord via webhooks.",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
